b57269b14c09e71a9a9a34b2073630970d27a6d3
kalash choudhary <choudharykalash1@gmail.com>
2024-09-10 19:23:12 +0530

    Button Configuration
    Created a button and did some configuration

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   var button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        
     }
 
 
+    func configButton() {
+        
+        button.setTitle("Tap", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .red
+        
+    }
 }
 
